e1e1e0add79417b22387f62e956d8ddae3523e23
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-10-23 01:01:45 +0300

    open window

--- src/Snake.c
+++ src/Snake.c
@@ -1,5 +1,6 @@
 #include "snake.h"
 #include <SDL.h>
+#include <SDL_render.h>
 #include <SDL_video.h>
 
 #define WINDOW_X 0
@@ -13,7 +14,7 @@ int WinMain(int args, int **argv) {
 
 
     SDL_Window *window;
-    SDL_Renderer *rendere;
+    SDL_Renderer *renderer;
 
     if (SDL_INIT_VIDEO < 0) {
         fprintf(stderr, "ERROR: SDL_INIT_VIDEO");
@@ -25,6 +26,26 @@ int WinMain(int args, int **argv) {
                             WINDOW_WIDTH,
                             WINDOW_HEIGHT,
                             SDL_WINDOW_BORDERLESS);
+
+    if (!window) {
+        fprintf(stderr, "ERROR: window not create");
+    }
+
+    renderer = SDL_CreateRenderer(window, -1 , SDL_RENDERER_ACCELERATED);
+
+    if (!renderer) {
+        fprintf(stderr, "ERROR: no renderer");
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0x00, 0xff, 0x00, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(2000);
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
     return 0;
 }
